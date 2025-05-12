e74d1ecbe452deef5a5037f217e438fc516ac7ad
Shyamsunder Tard <iamshyamsundertard@gmail.com>
2025-05-12 19:16:29 +0530

    feat: add icons component for UI elements

---
+++ src/components/Icons.tsx
@@ -0,0 +1,27 @@
+import { 
+  FiUpload, 
+  FiChevronDown, 
+  FiX, 
+  FiVideo,
+  FiLink,
+  FiCamera
+} from 'react-icons/fi';
+import { 
+  SiDropbox,
+  SiFacebook,
+  SiGoogledrive
+} from 'react-icons/si';
+import { 
+  AiOutlineGoogle
+} from 'react-icons/ai';
+
+export const UploadIcon = FiUpload;
+export const ChevronDownIcon = FiChevronDown;
+export const XIcon = FiX;
+export const DeviceIcon = FiVideo;
+export const LinkIcon = FiLink;
+export const CameraIcon = FiCamera;
+export const DropboxIcon = SiDropbox;
+export const FacebookIcon = SiFacebook;
+export const GoogleDriveIcon = SiGoogledrive;
+export const GooglePhotosIcon = AiOutlineGoogle;
